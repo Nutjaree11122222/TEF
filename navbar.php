696c491859add99a0709949552091a21dd17054a
Nutjaree11122222 <nutjareelungpon096@gmail.com>
2023-01-12 11:35:12 +0700

    TEF01

--- navbar.php
+++ navbar.php
@@ -5,7 +5,7 @@
         <a class="nav-link active" href="index.php">HOME</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="show_provice.php">Province</a>
+        <a class="nav-link" href="show_province.php">Province</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="show_customer.php">Customer</a>
