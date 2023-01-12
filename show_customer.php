bd4465501640f2d735539b9e8db188ff9628ccda
nutchaya19 <nutchaya0712@gmail.com>
2023-01-12 11:35:39 +0700

    eye

---
+++ show_customer.php
@@ -0,0 +1,48 @@
+<?php
+include ("header.php");
+include ("banner.php");
+include ("navigater.php");
+include ("connect.php");
+?>
+
+<div class = "container mt-5">
+        <h2><center>ข้อมูล Customer </center></h2>
+        <table class = "table table-bordered">
+            <tr>
+                <th>รหัสนักเรียน</th>
+                <th>ชื่อนักเรียน</th>
+                <th>กลุ่มการเรียน</th>
+                <th>ระดับชั้น</th>
+            </tr>
+    <?php
+    $sql = "SELECT * FROM customer";
+    $result = mysqli_query($conn, $sql);
+    
+    if (mysqli_num_rows($result) > 0) {
+      // output data of each row
+      while($row = mysqli_fetch_assoc($result)) {
+        
+        ?>
+            <tr>
+                <td><?php echo $row['cus_id'] ?></td>
+                <td><?php echo $row['cus_name'] ?></td>
+                <td><?php echo $row['cus_address'] ?></td>
+                <td><?php echo $row['cus_position'] ?></td>
+                <td><?php echo $row['cus_salary'] ?></td>
+            </tr>
+        <?php
+
+      }
+    } else {
+      echo "0 results";
+    }
+    
+    mysqli_close($conn);
+    ?>
+        </table>
+
+  </div>
+</div>
+<?php
+include ("footer.php");
+?>
