c0d025e9c179ab0a7c4209c9f1811d388106dbb0
Nutjaree11122222 <nutjareelungpon096@gmail.com>
2023-01-12 11:57:15 +0700

    TEF01

--- index.php
+++ index.php
@@ -2,3 +2,14 @@
 <?php include 'banner.php' ?>
 <?php include 'navbar.php' ?>
 <?php include 'connect.php' ?>
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>Group TEF</h2>
+      
+    </div>
+    <div class="col-sm-8">
+      <h2>TITLE HEADING</h2>
+    </div>
+  </div>
+</div>
